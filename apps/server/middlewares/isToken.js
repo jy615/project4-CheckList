2d2345502815788359f67695df77e23a09df120d
leejiayi <leejiayi615@gmail.com>
2022-12-16 06:23:57 +0800

    front end code

--- apps/server/middlewares/isToken.js
+++ apps/server/middlewares/isToken.js
@@ -1,24 +1,26 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-
 // token stored in local storage
 
-const isToken =  (req, res, next) => {
-    try {
-        //get token from header
-        const token = req.header("token");
-        if (!token) {
-            return res.status(401).json("Not Authorize");
-
-        }
-        // verify token
-        const payload = jwt.verify(token, process.env.JWT_SECRET)
-        req.user = payload.user
-    } catch (err) {
-        console.log(err.message);
-        return res.status(401).json("Server Error")
+const isToken = (req, res, next) => {
+  try {
+    //get token from header
+    // const token = req.header("token");
+    // console.log(req.headers.authorization);
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      return res.status(401).json("Not Authorize");
     }
-}
+    // // verify token
+    const payload = jwt.verify(token, process.env.JWT_SECRET);
+    // console.log(payload);
+    req.user = payload.user;
+    next();
+  } catch (err) {
+    console.log(err.message);
+    return res.status(401).json("Not Authorize");
+  }
+};
 
-module.exports = isToken
+module.exports = isToken;
--- apps/server/routes/patientList.js
+++ apps/server/routes/patientList.js
@@ -2,95 +2,137 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../db");
-const isUser = require ("../middlewares/isUser.js")
-const isToken = require("../middlewares/isToken.js")
-const jwtGenerator = require("../utils/jwtGenerator.js")
+const isUser = require("../middlewares/isUser.js");
+const isToken = require("../middlewares/isToken.js");
+const jwtGenerator = require("../utils/jwtGenerator.js");
 //* bcrypt dependencies
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
 //* dotenv
 require("dotenv").config();
 
-router.get("/", async(req,res)=> {
-    try {
-     //req.user return uuid
-        const user = await pool.query('SELECT * FROM users WHERE user_id = $1', [req.user])
-        console.log(user.rows[0])
-        return res.status(201).json(user.rows[0])
-       
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json("Server Error")
-    }
-})
+router.get("/", isToken, async (req, res) => {
+  try {
+    //req.user return uuid
+    // console.log(req.user);
+    const user = await pool.query("SELECT * FROM users WHERE id = $1", [
+      req.user,
+    ]);
+    // console.log(user.rows[0]);
+    return res.status(200).json(user.rows[0]);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("Server Error");
+  }
+});
 
 // get all patients
-router.get("/get", async (req, res)=> {
-    try {
-        const getPatient = await pool.query("SELECT * FROM patient");
-        return res.json(getPatient.rows)
-    } catch (error) {
-        console.log("error")
-    }})
+router.get("/get", isToken, async (req, res) => {
+  try {
+    const getPatient = await pool.query("SELECT * FROM patient");
+    return res.json(getPatient.rows);
+  } catch (error) {
+    console.log("error");
+  }
+});
 
-    // get 1 patient
-    router.get("/get/:id", async (req, res)=> {
-        try {
-            const {id} = req.params;
-            const getPatientById = await pool.query("SELECT * FROM patient WHERE id = $1", [id]);
-            res.json(getPatientById.rows[0])
-        } catch (err) {
-            console.log(err.message)
-        }})
+// get 1 patient
+router.get("/get/:id", isToken, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const getPatientById = await pool.query(
+      "SELECT * FROM patient WHERE id = $1",
+      [id]
+    );
+    res.json(getPatientById.rows[0]);
+  } catch (err) {
+    return res.status(500).json("Server Error");
+  }
+});
 
-    // get 1 patient from 2 tables
-    router.get("/get/:id", async (req, res)=> {
-        try {
-            const {id} = req.params;
-            const getPatientById = await pool.query("SELECT * FROM patient WHERE id = $1", [id]);
-            res.json(getPatientById.rows[0])
-        } catch (err) {
-            console.log(err.message)
-        }})
-    // update patient date and time
-router.put("/update/:id", async (req, res)=> {
-    try {
-        const {id} = req.params;
-        const {patient_name, patient_ic,  patient_date, patient_time, patient_status, procedure_id} = req.body
-        const updatePatient = await pool.query('UPDATE patient SET patient_date = $1, patient_time = $2, patient_procedure = $3 WHERE id = $4',[patient_date, patient_time, patien_procedure, id])
-    } catch (err) {
-        console.error(err.message)
-    }
-})
+// get 1 patient from 2 tables
+// router.get("/get/:id", async (req, res) => {
+//   try {
+//     const { id } = req.params;
+//     const getPatientById = await pool.query(
+//       "SELECT * FROM patient WHERE id = $1",
+//       [id]
+//     );
+//     res.json(getPatientById.rows[0]);
+//   } catch (err) {
+//     console.log(err.message);
+//   }
+// });
+// update patient date and time
+router.put("/update/:id", isToken, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      patient_name,
+      patient_ic,
+      patient_date,
+      patient_time,
+      patient_status,
+      procedure_id,
+    } = req.body;
+    const updatePatient = await pool.query(
+      "UPDATE patient SET patient_date = $1, patient_time = $2 WHERE id = $3",
+      [patient_date, patient_time, id]
+    );
+
+    res.json("Patient updated");
+  } catch (err) {
+    return res.status(500).json("Server Error");
+  }
+});
+
+router.delete("/delete/:id", isToken, async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // console.log(id);
+    await pool.query("DELETE FROM patient WHERE id = $1", [id]);
+
+    res.json("Patient deleted");
+  } catch (error) {
+    return res.status(500).json("Server Error");
+  }
+});
 
 // get all procedure
-router.get("/procedure/get", async (req, res)=> {
-    try {
-        const getProcedure = await pool.query("SELECT * FROM procedure");
-        return res.json(getProcedure.rows)
-    } catch (error) {
-        console.log("error")
-}
-})
+router.get("/procedure/get", isToken, async (req, res) => {
+  try {
+    const getProcedure = await pool.query("SELECT * FROM procedure");
+    return res.json(getProcedure.rows);
+  } catch (error) {
+    console.log("error");
+  }
+});
 // update patient procedure
-router.put("/procedure/:id", async (req, res)=> {
-    try {
-        const {id} = req.params;
-        const {procedure_name} = req.body
-        const updateProcedure = await pool.query('UPDATE procedure SET procedure_name = $1 WHERE id = $2',[procedure_name, id])
-    } catch (err) {
-        console.error(err.message)
-    }
-})
+router.put("/procedure/update/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { procedure_name } = req.body;
+    const updateProcedure = await pool.query(
+      "UPDATE procedure SET procedure_name = $1 WHERE id = $2",
+      [procedure_name, id]
+    );
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // delete patient procedure
-router.delete("/delete/:id", async (req, res)=> {
-    try {
-        const {id} = req.params;
-        const deletePatient = await pool.query('DELETE FROM procedure WHERE id = $1',[id])
-        res.json("Patient was deleted")
-    } catch (err) {
-        console.error(err.message)
-    }
-})
+router.delete("/procedure/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletePatient = await pool.query(
+      "DELETE FROM procedure WHERE id = $1",
+      [id]
+    );
+    res.json("Patient was deleted");
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 module.exports = router;
